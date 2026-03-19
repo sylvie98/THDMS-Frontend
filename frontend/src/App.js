3d31e4ba7f3fc6dfcc2d28f418b9133e5b48ad44
sylvie98 <sylviemasengesho5@gmail.com>
2026-03-19 12:26:40 +0200

    install tailwind css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>TEA HARVEST DIGITAL MANAGEMENT SYSTEM </h2>
+      <h2 className='bg-green-500 text-white p-4 '>TEA HARVEST DIGITAL MANAGEMENT SYSTEM </h2>
     </div>
   );
 }
